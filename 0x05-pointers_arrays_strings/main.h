40e28bff3abd79463c43a282d79d84bbbc7a83b2
Nwachiemu <emuchaynwachi@gmail.com>
2023-08-21 14:53:31 -0700

    reset

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,7 @@
-#ifndef MAIN_H
-#ifndef MAIN_H
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
-#include "main.h"
-#include <stdio.h>
-
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
