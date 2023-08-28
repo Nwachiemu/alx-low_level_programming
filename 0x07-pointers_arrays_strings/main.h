05ebd6cdd1a28bea4587986e4683587a74ecd4a7
Nwachiemu <emuchaynwachi@gmail.com>
2023-08-28 16:15:20 -0700

    chess

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,4 +1,3 @@
-
 #ifndef FILE_MAIN
 #define FILE_MAIN
 
@@ -6,6 +5,7 @@ char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
+int _putchar(char c);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
