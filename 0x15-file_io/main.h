3a81246c66ca15b75fc3966b5ef74844859a813b
Nwachiemu <emuchaynwachi@gmail.com>
2023-10-03 13:22:15 +0100

    modified main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/*
+ *  * File: main.h
+ *   * Auth: Emuchay Nwachi
+ *    * Desc: Header file containing prototypes for all functions
+ *     *       written in the 0x14-file_io directory.
+ *      */
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
